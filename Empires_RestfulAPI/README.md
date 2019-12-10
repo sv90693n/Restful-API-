467b17e19a3327640e50faf004d5bf1985f388eb
ravitejagrt <ravitejagrt@users.noreply.github.com>
2019-12-10 16:21:10 -0500

    Add files via upload

---
+++ Empires_RestfulAPI/README.md
@@ -0,0 +1,28 @@
+#RESTful webservice using Docker
+
+Created a RESTful Web service that displays data of empires in JSON format using two get routes: 
+   
+* /empires
+   
+* /empires/empireId
+    
+Steps to run RESTful web service:
+
+* Create the project with myapp.py and JSON file in a single folder.
+  The templates folder consists of the page.html file which displays the json result on the webpage
+
+* A Docker Image is created by running the following command: -
+ "docker build -t empires-image:latest ."
+
+* You can check the image created using "docker images". You will find the image students-image under the list.
+
+* Run the created students-image image inside a container using the following command
+  "docker run -d -p 5001:5000 --name empires-container empires-image" 
+
+* Check running docker container by "docker ps"
+
+* App will now be running at the following address: -
+  http://0.0.0.0:5001/empires
+  
+* For specific Id use the following url
+  http://0.0.0.0:5001/empires/1001 --- 1001 is the id you wish to search
---
+++ Empires_RestfulAPI/app.py
@@ -0,0 +1,66 @@
+import json
+from flask import Flask, jsonify, render_template, request, Response
+
+
+app = Flask(__name__)
+
+
+with open('EmpiresArmy.JSON') as json_data:
+    d = json.load(json_data)
+    list_of_empires = []
+    for data in d['empires']:
+    	list_of_empires.append(data)
+
+with open('Army.JSON') as army_data:
+	a = json.load(army_data)
+	list_of_armies = []
+	for army in a['armies']:
+		list_of_armies.append(army)
+	print("list_of_armies: ", list_of_armies)
+
+@app.route('/empires/<string:empire_id>/army', methods =['GET'])
+def army_by_empire_id(empire_id):
+	empire_army = [armies for armies in list_of_armies if armies['empireid'] == empire_id]
+	# print("empire_army: ", str(empire_army))
+	army = empire_army[0]['army']
+	# print("empire_army: ", str(army))
+	typefilter = request.args.get('type')
+	if (typefilter == 'json'):
+		return jsonify(empire_army)
+	return render_template("index.html", list_army_data=army)
+
+@app.route('/', methods =['GET'])
+def home():
+	return render_template("index.html")
+
+@app.route('/empires', methods =['GET'])
+def all_empires():
+	typefilter = request.args.get('type')
+	if (typefilter == 'json'):
+		return jsonify(d)
+	return render_template("index.html",list_data=list_of_empires)
+
+@app.route('/empires/<string:empire_id>', methods =['GET'])
+def empire_by_id(empire_id):
+	emp = [empires for empires in list_of_empires if empires['empireid'] == empire_id]
+	typefilter = request.args.get('type')
+	if (typefilter == 'json'):
+		return jsonify(emp)
+	return render_template("index.html",list_data=emp)
+
+
+@app.route('/empires/<string:empire_id>/army/<string:army_id>', methods =['GET'])
+def army_by_empire_id_army_id(empire_id, army_id):
+	empire_army = [armies for armies in list_of_armies if armies['empireid'] == empire_id]
+	# print("empire_army: ", str(empire_army))
+	# print("empire_army[0]: ", str(empire_army[0]))
+	# print("army: ", str(empire_army[0]['army']))
+	arm = [army for army in empire_army[0]['army'] if army['armyid'] == army_id]
+	print("single arm: ", arm)
+	typefilter = request.args.get('type')
+	if (typefilter == 'json'):
+		return jsonify(arm)
+	return render_template("index.html", list_army_data=arm)
+
+if __name__ == '__main__':
+	 app.run(host='0.0.0.0', port=5000)
